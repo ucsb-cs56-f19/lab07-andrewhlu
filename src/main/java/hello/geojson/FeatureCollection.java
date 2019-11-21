02daf3b4700a49dd8e7ac605577f76e53320903e
andrewhlu <alu@ucsb.edu>
2019-11-20 22:17:39 -0800

    AL - Create table to display earthquakes (features)

--- src/main/java/hello/geojson/FeatureCollection.java
+++ src/main/java/hello/geojson/FeatureCollection.java
@@ -7,10 +7,12 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.List;
+
 public class FeatureCollection {
     public String type;
-
     public Metadata metadata;
+    public List<Feature> features;
 
     private static Logger logger = LoggerFactory.getLogger(FeatureCollection.class);
 
